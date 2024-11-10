fd3b6b195a789197d6d40ea33420872f982a4a28
Paul Rakutt <keksgauner@gmail.com>
2024-11-10 21:49:26 +0100

    todo routes

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,20 @@ Route::get('/calculator', function () {
     return view('calculator');
 })->middleware(['auth'])->name('calculator');
 
-Route::resource('todos', TodoController::class)->middleware(['auth']);
+Route::middleware(['auth'])->prefix('todos')->group(function () {
+    Route::get('/', [TodoController::class, 'index'])->name('todos.index');
+    Route::get('/create', [TodoController::class, 'create'])->name('todos.create');
+    Route::post('/', [TodoController::class, 'store'])->name('todos.store');
+    Route::get('/{todo}/edit', [TodoController::class, 'edit'])->name('todos.edit');
+    Route::put('/{todo}', [TodoController::class, 'update'])->name('todos.update');
+    Route::delete('/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
+});
 
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->prefix('profile')->group(function () {
+    Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 
